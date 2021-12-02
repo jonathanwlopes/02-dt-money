de5190cd456de18eb0800c45ac369f8255c73431
jonathanwlopes <jonathan@agenciaeplus.com.br>
2021-12-02 08:57:30 -0300

    insert fake api

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,18 @@
 import * as S from "./styles"
 import logoImage from "../../assets/logo.svg"
 
-export const Header = () => {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void
+}
+
+export const Header = ({ onOpenNewTransactionModal }: HeaderProps) => {
   return (
     <S.Container>
       <S.Content>
         <S.Logo src={logoImage} alt="dt money" />
-        <S.Button type="button">Nova Transação</S.Button>
+        <S.Button type="button" onClick={onOpenNewTransactionModal}>
+          Nova Transação
+        </S.Button>
       </S.Content>
     </S.Container>
   )
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react"
+import { api } from "../../services/api"
 import * as S from "./styles"
 
 export const TransactionsTable = () => {
+  useEffect(() => {
+    api.get("/transactions").then((response) => console.log(response.data))
+  }, [])
+
   return (
     <S.Container>
       <S.Table>
